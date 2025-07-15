03a03acb07265da47abcddb1f31df0e8b0b13be5
omar ashraf <omar.ashraf1182005@gmail.com>
2025-07-15 11:58:48 +0300

    abstracted input handler class from from command line UI

--- src/main.cpp
+++ src/main.cpp
@@ -4,9 +4,14 @@
 class InputHandler{
 private:
     //this is made to stop the user from entering 10000 element string and causing stack overflow
-    static int constexpr MAX_RAW_INPUT_LENGTH = 5;
-    static int constexpr MAX_MENU_ITEMS = 5; 
-    static int getInput(){
+    static int constexpr MAX_RAW_INPUT_LENGTH = 10;
+    int const menuItemSize; 
+
+public:
+    static constexpr int INVALID_INPUT = -1;
+
+    InputHandler(const int numberOfMenuItems): menuItemSize(numberOfMenuItems){}
+    int getUserOperation(){
         std::string input = "", effectiveInput = "";
 
         getline(std::cin, input);
@@ -27,34 +32,29 @@ private:
         //if max input changed in the future, this will prevent out of range input
         try{
             int choice = std::stoi(effectiveInput);
-            return (choice >= 1 && choice <= MAX_MENU_ITEMS) ? choice : INVALID_INPUT;
+            return (choice >= 1 && choice <= menuItemSize) ? choice : INVALID_INPUT;
         }catch(...){
             return INVALID_INPUT;
         }
 
     }
 
-public:
-    static constexpr int INVALID_INPUT = -1;
-    static int getUserOperation(){
-        std::cout<<"**********************************************************"<<std::endl<<std::endl;
-        std::cout<<"        Welcome to smart Home Automation Assistant"<<std::endl<<std::endl;
-        std::cout<<"**********************************************************"<<std::endl<<std::endl;
-
-        std::cout<<"1) create device"<<std::endl;
-        std::cout<<"2) list device info"<<std::endl;
-        std::cout<<"3) list devices' info"<<std::endl;
-        std::cout<<"4) remove device"<<std::endl;
-        std::cout<<"5) exit"<<std::endl<<std::endl;
-        
-        std::cout<<"Choose operation: ";
-        return getInput();
-    }
-
 };
 
 int main(){
-    int input = InputHandler::getUserOperation();
+    InputHandler firstInterface(5);
+    std::cout<<"**********************************************************"<<std::endl<<std::endl;
+    std::cout<<"        Welcome to smart Home Automation Assistant"<<std::endl<<std::endl;
+    std::cout<<"**********************************************************"<<std::endl<<std::endl;
+
+    std::cout<<"1) create device"<<std::endl;
+    std::cout<<"2) list device info"<<std::endl;
+    std::cout<<"3) list devices' info"<<std::endl;
+    std::cout<<"4) remove device"<<std::endl;
+    std::cout<<"5) exit"<<std::endl<<std::endl;
+    
+    std::cout<<"Choose operation: ";
+    int input = firstInterface.getUserOperation();
     if(input == InputHandler::INVALID_INPUT)
         std::cout<<"input a valid choice within range"<<std::endl;
 }
